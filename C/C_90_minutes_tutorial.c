a3bebb20527a09fc4509b8ff8835c15db12a5a72
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-07-21 19:35:09 +0200

    Started on the C 90 minute tutorial.

---
+++ C/C_90_minutes_tutorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    //Output
+    printf("Hello World! \n");
+    printf("Hallo Welt!");
+
+    //empty Line
+    printf("\n");
+
+    //Variablen
+    char* receiver = "Herr Mayer";
+    int attachments = 2;
+    printf("Hallo, %s", receiver);
+
+    return 0;
+}
