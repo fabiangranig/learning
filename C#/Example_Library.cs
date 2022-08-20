d4c6030d304699e6a6365bc6f0ddc2f3a2b79c4e
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-08-20 12:14:34 +0200

    Added Library Example

---
+++ C#/Example_Library.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lib
+{
+    public class Method
+    {
+        public static int Plus_Number(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ C#/Testing_Library.cs
@@ -0,0 +1,13 @@
+using System;
+using static Lib.Method;
+
+class Klasse
+{
+    public static void Main(string[] args)
+    {
+        int a = 3;
+        int b = 4;
+        int c = Plus_Number(a, b);
+        Console.WriteLine(c);
+    }
+}
