09741789e4e143106fe92b27ece8b4b6200ee8f1
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-12-23 20:03:07 +0100

    Added template

--- Python/AI/tutorial/test.py
+++ Python/AI/tutorial/test.py
@@ -1,1 +1,15 @@
-# empty
+# import enviroment
+import gym
+from gym.envs import box2d
+env = gym.make("LunarLander-v2", render_mode="humen")
+env.action_space.seed(42)
+
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
